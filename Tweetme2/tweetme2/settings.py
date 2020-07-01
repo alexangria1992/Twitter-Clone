f21abd0a3907a237f075ccd80ac646a242a7aca2
alexangria1992 <55860445+alexangria1992@users.noreply.github.com>
2020-07-01 20:09:23 +1200

    Safe URL Redirect

--- Tweetme2/tweetme2/settings.py
+++ Tweetme2/tweetme2/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'uj1*4mbm&4#dv8sre%!dj^9&&czpmf^(gps))qq%+o!z9o_jh3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
