1a7a921be3c8e3a6abc3cdb9aa964e4495854cbf
alexangria1992 <55860445+alexangria1992@users.noreply.github.com>
2020-07-03 19:45:53 +1200

    Error stuck on delete view

--- Tweetme2/tweets/views.py
+++ Tweetme2/tweets/views.py
@@ -3,11 +3,14 @@ from django.conf import settings
 from django.http import HttpResponse, Http404, JsonResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.utils.http import is_safe_url
-from rest_framework.decorators import api_view
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from .forms import TweetForm
 from .models import Tweet
 from .serializer import TweetSerializer
+
 ALLOWED_HOSTS = settings.ALLOWED_HOSTS
 # Create your views here.
 def home_view(request, *args, **kwargs):
@@ -16,7 +19,9 @@ def home_view(request, *args, **kwargs):
     print(request.user)
     return render(request, "pages/home.html", context={}, status=200)
    
-@api_view(['POST']) #http method the client == POST
+@api_view(['POST'])  #http method the client == POST
+#@authentication_classes([SessionAuthentication, MyCustomAuth])
+@permission_classes([IsAuthenticated])
 def tweet_create_view(request, *args, **kwargs):
     serializer = TweetSerializer(data = request.POST)
     if serializer.is_valid(raise_exception=True):
@@ -33,6 +38,20 @@ def tweet_detail_view(request, tweet_id, *args, **kwargs):
        obj = qs.first()
        serializer = TweetSerializer(obj)
        return Response(serializer.data, status=200)
+
+
+@api_view(['delete', 'POST'])  #
+@permission_classes([IsAuthenticated])
+def tweet_delete_view(request, tweet_id, *args, **kwargs):
+       qs = Tweet.objects.filter(id=tweet_id)
+       if not qs.exists():
+           return Response({}, status=404)
+       qs = qs.filter(user=request.user)
+       if not qs.exists():
+           return Response({"message": "You cannot delete this tweet"}, status=401)
+       obj = qs.first()
+       obj.delete()
+       return Response({"message": "Tweet removed"}, status=200)
        
 @api_view(['GET']) #
 def tweet_list_view(request, *args, **kwargs):
